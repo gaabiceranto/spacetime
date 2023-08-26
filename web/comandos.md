a2956c6a8190966b256ff5dcbc5aeb6aef111ef2
Gabriela Ceranto <gabiceranto@gmail.com>
2023-08-26 16:44:01 -0300

    fix: reverte commit

---
+++ web/comandos.md
@@ -0,0 +1,5 @@
+Adiciona as configurações do ESLINT da rocket aqui no projeto web-next
+> npm i @rocketseat/eslint-config -D
+
+
+> npm i prettier-plugin-tailwindcss -D
---
+++ web/src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return <div className="h-screen bg-zinc-950 text-zinc-50 p-5">
+    <h1 className="text-4xl font-alt">Sua cápsula do tempo</h1>
+    <h1 className="text-4xl font-sans">Sua cápsula do tempo</h1>
+  </div>
+}
