6ade67055a3d7340e3e10270a77819a9bb2dbbd7
gaabiceranto <gabiceranto@gmail.com>
2023-12-18 12:05:53 -0300

    feat: Adjust Create Github Client ID

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,53 +1,31 @@
-import Image from 'next/image'
-import {User} from 'lucide-react'
-import nlwlogo from '../assets/nlw-spacetime-logo.svg'
+import Copyright from '@/components/Copyright'
+import { EmptyMemories } from '@/components/EmptyMemories'
+import Hero from '@/components/Hero'
+import SignIn from '@/components/SignIn'
 
 export default function Home() {
   return <div className="grid min-h-screen grid-cols-2">
-    
+
     {/* Left */}
     <div className='relative flex flex-col items-start justify-between px-28 py-16  overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover'>
-     
-     {/* Blur */}
-      <div className="absolute rigth-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2  bg-purple-700 opacity-50 rounded-full blur-full"/>
-     
+
+      {/* Blur */}
+      <div className="absolute rigth-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2  bg-purple-700 opacity-50 rounded-full blur-full" />
+
       {/* Stripes */}
-      <div className='absolute right-1.5 top-0 bottom-0 w-2 bg-stripes '/>
+      <div className='absolute right-1.5 top-0 bottom-0 w-2 bg-stripes ' />
 
-      {/* Sign In */}
-      <a href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`} className='flex items-center gap-3 text-left hover:text-gray-50'>
-        <div className='flex h-10 w-10 items-center justify-center rounded-full bg-gray-400'>
-          <User className='h-5 w-5 text-gray-500'/>
-        </div>
-        <p className='text-sm leading-snug max-w-[140px]'><span className='underline'>Crie sua conta</span> e salve suas memórias!</p>
-      </a>
+      <SignIn />
 
-      {/* Hero */}
-      <div className='space-y-5'>
-        <Image src={nlwlogo} alt='NLW Spacetime Logo'/>
-        <div className='max-w-[420px] space-y-2'>
+      <Hero />
 
-          <h1 className='mt-5 text-5xl font-bold leading-tight text-gray-50'>Sua cápsula do tempo</h1>
-          <p className='text-lg leading-relaxed'>Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</p>
-
-          <a className='inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black  hover:bg-green-600' href="">CADASTRAR LEMBRANçA</a>
-        </div>
-      </div>
-
-      {/* Copyright */}
-      <div className='text-sm leading-relaxed text-gray-200'>Feito com 💜 no NLW da {' '} <a target='_blank' rel='noreferrer' className='underline hover:text-gray-100' href="https://rocketseat.com.br">Rocketseat</a>
-
-      </div>
-      
-
+      <Copyright />
 
     </div>
 
     {/* Right */}
     <div className=' flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover'>
-      <div className='flex flex-1 items-center justify-center'>
-        <p className='text-center leading-relaxed w-[360px]'>Você ainda não registrou nenhuma lembrança, comece a{' '} <a href="#" className='underline hover:text-gray-50'>criar agora!</a></p>
-      </div>
+      <EmptyMemories />
     </div>
 
   </div>
